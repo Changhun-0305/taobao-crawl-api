f9d8558ee61bb32f8825c182b88a1b5e4ccb9eca
Changhun-0305 <changhun0305@gmail.com>
2020-05-18 23:03:35 +0900

    add testing, gitignore

--- api/taobao/tests.py
+++ api/taobao/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.test import Client
+import json
 
 # Create your tests here.
+class TaobaoTest(TestCase):
+    def setUp(self):
+        client = Client()
+        response = client.get('/taobao/?id=565838198470')
+        self.json_data = json.loads(response.content.decode('utf-8'))
+
+    def test_product_name(self):
+        self.assertEqual(self.json_data['title'], '벨벳 야구 유니폼, 실제 보관. 지퍼 재킷')
+    
+    def test_product_price(self):
+        self.assertEqual(self.json_data['price'], '315.00')
+    
+    def test_product_options(self):
+        self.assertEqual(self.json_data['options'], [{'크기': ['에스', '미디엄', '엘', '특대']}, {'색상 분류': ['회색', '분홍']}])
--- api/taobao/views.py
+++ api/taobao/views.py
@@ -11,20 +11,23 @@ import json
 URL = "https://item.taobao.com/item.htm?id="
 
 @api_view(['GET'])
-def get_product(request):
+def get_product_by_id(request):
     info = {}
 
     chrome_dir = dirname(dirname(abspath(__file__))) + "/chromedriver"
     custom_options = webdriver.ChromeOptions()
     prefs = {
-    "translate_whitelists": {"zh-CN":"ko"},
-    "translate":{"enabled":"true"}
+        "translate_whitelists": {"zh-CN":"ko"},
+        "translate":{"enabled":"true"}
     }
     custom_options.add_experimental_option("prefs", prefs)
+    #custom_options.add_argument('headless')
     driver=webdriver.Chrome(chrome_dir, options=custom_options)
-    driver.implicitly_wait(3)
+    driver.implicitly_wait(5)
     product_url =URL + request.query_params['id']
     driver.get(product_url)
+
+    #accept alert
     try :
         driver.switch_to_alert().accept()
     except:
@@ -38,14 +41,15 @@ def get_product(request):
     info["options"] = []
     options = []
     option_tags = soup.find_all("dl", class_="J_Prop")
+
     for option_tag in option_tags:
-        
         option_name = option_tag.dt.text
         option_dict = {option_name: []}
         for li in option_tag.dd.ul.findAll('li'):
             option_dict[option_name].append(li.a.text.lstrip('\n').rstrip('\n').strip())
         options.append(option_dict)
+
     info["options"] = options
     driver.close()
-    info_json = json.dumps(info)
-    return HttpResponse(info_json)
+    info_json = json.dumps(info, ensure_ascii=False)
+    return HttpResponse(info_json, content_type=u"application/json; charset=utf-8")
